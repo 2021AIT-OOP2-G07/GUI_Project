7ed20447c03184a402f909b4319553d3a9e7c292
do06ami <95838842+do06ami@users.noreply.github.com>
2022-01-26 19:17:57 +0900

    /select直し＋js

--- templates/count.js
+++ templates/count.js
@@ -1,11 +1,14 @@
-var count = 3;
-    var countdown = function(){
-        document.getElementById('timer').testContent = count.toString();
-        console.log(count--);
-        var id = setTimeout(countdown, 1000);
-        if(count < 0){
-            clearTimeout(id);
-        }
+var PassSec=10;   // 秒数カウント用変数
+ 
+    // 繰り返し処理の中身
+    function showPassage() {
+        PassSec--;   // カウントアップ
+        var msg = "撮影開始まで残り " + PassSec + "秒！。";   // 表示文作成
+        document.getElementById("timer").innerHTML = msg;   // 表示更新
     }
-    countdown();
+    // 繰り返し処理の開始
+    function startShowing() {
+        PassageID = setInterval('showPassage()',1000);
+    }
+    startShowing();
 
